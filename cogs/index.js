8a7a0f658aca10365716d9741843ffadadb5075a
b1nary-b1nzy <42985463+b1nary-b1nzy@users.noreply.github.com>
2018-09-04 19:38:59 -0500

    cogs pt1

---
+++ cogs/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    BloodyClient: require("./BloodyClient.js"),
+    Command: require("./structures/Command.js"),
+    Inhibitor: require("./structures/Inhibitor"),
+    PaginateMenu: require("./structures/PaginateMenu.js"),
+    version: require("../package.json").version
+};
