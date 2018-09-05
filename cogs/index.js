8bdc7f6390061f43ad63962aec59a517c1f6d043
b1nary-b1nzy <42985463+b1nary-b1nzy@users.noreply.github.com>
2018-09-04 19:39:16 -0500

    Delete index.js

--- cogs/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    BloodyClient: require("./BloodyClient.js"),
-    Command: require("./structures/Command.js"),
-    Inhibitor: require("./structures/Inhibitor"),
-    PaginateMenu: require("./structures/PaginateMenu.js"),
-    version: require("../package.json").version
-};
