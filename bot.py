7bad6cd2556b611be0838ed09c5b7e31280312b6
b1nary-b1nzy <42985463+b1nary-b1nzy@users.noreply.github.com>
2018-09-04 19:12:12 -0500

    heroku

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+import aiohttp
+import json
+import random
+import os
+import io
+
+client = commands.Bot(command_prefix='e.', owner_id=437150834888015872)
+client.session = aiohttp.ClientSession()
+client.commands_run = 0
+
+@client.event
+async def on_ready():
+  print('Logged in as:')
+  print(client.user.name)
+  print(client.user.id)
+  while True:
+    await client.change_presence(activity=discord.Game(type=discord.ActivityType.listening, name='for e.help!'))
+    await client.change_presence(activity=discord.Game(type=discord.ActivityType.listening, name=f'to {len(bot.guilds)} guilds!'))
+    
+client.run(os.environ['TOKEN'])
